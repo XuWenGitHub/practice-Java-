3d0356c88f3273639e03cad0a7f539c66171a83a
XuWenGitHub <380717413@qq.com>
2020-07-17 17:36:28 +0800

    整形提升和java基本类型和引用类型

---
+++ 2020_7_17/Demo.java
@@ -0,0 +1,31 @@
+
+public class Demo{
+	public static void main(String[] args){
+		short a=128;
+		byte b=(byte)(128);
+		//byte c=128;
+		System.out.println(b);
+		//int a=-12;
+		//long b=123;
+		//short c = 10;
+		//float d=1.0f;
+		//System.out.println(a<<2);
+		
+		
+		//switch(){}	小括号中不能是浮点型
+		//switch(b){
+			
+		//}
+		//short a=128;
+		//byte b=(byte)a;
+		//System.out.println(a+" "+b);
+		//double x=2.0;
+		//int y=4;
+		//x/=++y;
+		//System.out.println(x);
+		//byte a=130;	false
+		//int h=3;
+		//byte j=h;
+		
+	}
+}
