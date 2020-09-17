90f52986cccf551983a26679e6bd017f44d01c7c
XuWenGitHub <380717413@qq.com>
2020-09-17 18:36:42 +0800

    List集合实现扑克

---
+++ 2020_9_16/src/Player.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    public List<Card> cardList = new ArrayList<>();
+    public String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
---
+++ 2020_9_16/src/practice/one/Demo.java
@@ -0,0 +1,14 @@
+package practice.one;
+
+/**
+ * @PackgeName: practice.one
+ * @ClassName: Demo
+ * @Author: XuWen
+ * Date: 2020/9/16 18:53
+ * Introduce:
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
