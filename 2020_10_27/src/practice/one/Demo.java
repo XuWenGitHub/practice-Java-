d00a56a529da3a59b7f9ed7a8851e435f98578ad
XuWenGitHub <380717413@qq.com>
2020-10-28 23:43:25 +0800

    模拟数据库的表建类

---
+++ 2020_10_27/src/practice/one/Demo.java
@@ -0,0 +1,14 @@
+package practice.one;
+
+/**
+ * @PackgeName: practice.one
+ * @ClassName: Demo
+ * @Author: XuWen
+ * Date: 2020/10/27 18:28
+ * Introduce:
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
