0299d93310a922f61e0b435af068c6efc3a5c040
XuWenGitHub <380717413@qq.com>
2020-10-30 17:02:51 +0800

    mysql数据库练习

---
+++ 2020_10_30/src/practice/one/Demo.java
@@ -0,0 +1,18 @@
+package practice.one;
+
+/**
+ * @PackgeName: practice.one
+ * @ClassName: Demo
+ * @Author: XuWen
+ * Date: 2020/10/30 15:36
+ * Introduce:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        //update from book set price=61 where bookname='Java核心技术';
+        //update commodity set price=(price+50) where count>30;
+        //select * from user where ( (id>=1&&id<=200) || (id>=300&&id<=500) )&&accout is not null&&amount>1000;
+        //select * from book where author is not null or (price>50&&publish_date>2019_01_01 00:00:00;
+        //select * from article where create_date>=2010_01_01 10:30:00&&create_date<=2010_11_10 04:02:00
+    }
+}
