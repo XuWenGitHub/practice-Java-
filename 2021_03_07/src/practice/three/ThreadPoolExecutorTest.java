284310d28fbc413a24f2f26aba58682987801c40
XuWenGitHub <380717413@qq.com>
2021-03-08 22:17:53 +0800

    线程池总结

--- 2021_03_07/src/practice/three/ThreadPoolExecutorTest.java
+++ 2021_03_07/src/practice/three/ThreadPoolExecutorTest.java
@@ -1,9 +1,7 @@
 package practice.three;
 
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.TimerTask;
+import java.util.concurrent.*;
 
 public class ThreadPoolExecutorTest {
     public static void main(String[] args) {
@@ -13,16 +11,15 @@ public class ThreadPoolExecutorTest {
                 60,
                 TimeUnit.SECONDS,   //idle线程的空闲时间：临时工最大的存活时间，超过时间就解雇掉
                 new LinkedBlockingDeque<>(),//阻塞队列：任务存放的地方（快递仓库）
-                new ThreadFactory(){
+                new ThreadFactory() {
                     @Override
                     public Thread newThread(Runnable r) {
                         return new Thread(new Runnable() {
                             @Override
                             public void run() {
-                                long start = System.currentTimeMillis();
+                                System.out.println(1);
                                 r.run();
-                                long end = System.currentTimeMillis();
-                                System.out.println("任务执行了"+(end-start));
+                                System.out.println(Thread.currentThread().getName()+"开始执行了");
                             }
                         });
 
@@ -41,14 +38,37 @@ public class ThreadPoolExecutorTest {
             //线程池执行任务：execute、submit--->提交执行一个任务
             pool.execute(new Runnable() {
                 @Override
-                public void run() {
+                public void run() { //如果上面创建线程池，提供了工厂类参数，那么走上面的逻辑，除非，上面调用r.run()
                     try {
+                        System.out.println(Thread.currentThread().getName());
+                        System.out.println("--------------");
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             });
+        //线程池有4个快捷创建方式（实际工作不使用，作为面试要了解）
+        //阿里java开发手册，说明不能直接使用以下方式创建线程池
+        //实际工作需要使用ThreadPoolExecutor，构造参数自己指定
+        ExecutorService pool2 = Executors.newSingleThreadExecutor();    //单线程池
+        ExecutorService pool3 = Executors.newCachedThreadPool();    //缓存的线程池
+        ScheduledExecutorService pool4 = Executors.newScheduledThreadPool(4);    //计划任务线程池
+        ExecutorService pool5 = Executors.newFixedThreadPool(4);    //固定大小的线程池
+
+        pool4.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("hello");
+            }
+        }, 2, TimeUnit.SECONDS);  //2秒后，开始
+
+        pool4.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("闹钟");
+            }
+        }, 2, 1, TimeUnit.SECONDS);    //2秒钟后开始执行，每间隔1秒钟后，再运行一次
         }
     }
 }
